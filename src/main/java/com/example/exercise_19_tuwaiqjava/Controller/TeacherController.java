2eb7fa8fb921cdc1669f46021b899d458af5d1d7
aliali <aliali@test.com>
2025-04-16 23:54:07 +0300

    CRUD endpoint

--- src/main/java/com/example/exercise_19_tuwaiqjava/Controller/TeacherController.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Controller/TeacherController.java
@@ -1,4 +1,42 @@
 package com.example.exercise_19_tuwaiqjava.Controller;
 
+import com.example.exercise_19_tuwaiqjava.ApiResponse.ApiResponse;
+import com.example.exercise_19_tuwaiqjava.Model.Teacher;
+import com.example.exercise_19_tuwaiqjava.Service.TeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/teacher")
+@RequiredArgsConstructor
 public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAllTeachers(){
+        return ResponseEntity.status(200).body(teacherService.getAllTeachers());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addTeacher(@RequestBody @Valid Teacher teacher){
+        teacherService.addTeacher(teacher);
+        return ResponseEntity.status(200).body(new ApiResponse("Teacher is added"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateTeacher(@PathVariable Integer id,@RequestBody@Valid Teacher teacher){
+        teacherService.updateTeacher(id, teacher);
+        return ResponseEntity.status(200).body(new ApiResponse("Teacher is updated"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteTeacher(@PathVariable Integer id){
+        teacherService.deleteTeacher(id);
+        return ResponseEntity.status(200).body(new ApiResponse("teacher is deleted"));
+    }
+
+
 }
