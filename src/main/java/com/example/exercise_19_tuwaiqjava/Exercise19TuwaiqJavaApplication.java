916dbd82c6240a9f35f0f4c974b4a4185fb249da
aliali <aliali@test.com>
2025-04-16 03:29:33 +0300

    first commit

---
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Exercise19TuwaiqJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise_19_tuwaiqjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise19TuwaiqJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise19TuwaiqJavaApplication.class, args);
+    }
+
+}
