d38f931dca1f4900421d77032a46774b9ef14f7b
aliali <aliali@test.com>
2025-04-16 23:55:42 +0300

    DTO operations DTO class, DTO on address service and DTO parameter of methods in address controller

--- src/main/java/com/example/exercise_19_tuwaiqjava/Controller/AddressController.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Controller/AddressController.java
@@ -1,4 +1,31 @@
 package com.example.exercise_19_tuwaiqjava.Controller;
 
+
+import com.example.exercise_19_tuwaiqjava.ApiResponse.ApiResponse;
+import com.example.exercise_19_tuwaiqjava.DTO.AddressDTO;
+import com.example.exercise_19_tuwaiqjava.Service.AddressService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/address")
+@RequiredArgsConstructor
 public class AddressController {
+
+    private final AddressService addressService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAllAddresses(){
+        return ResponseEntity.status(200).body(addressService.getAllAddresses());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addAddress(@RequestBody@Valid AddressDTO addressDTO){
+        addressService.addAddress(addressDTO);
+        return ResponseEntity.status(200).body(new ApiResponse("address updated"));
+    }
+
 }
--- src/main/java/com/example/exercise_19_tuwaiqjava/DTO/AddressDTO.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/DTO/AddressDTO.java
@@ -1,4 +1,20 @@
 package com.example.exercise_19_tuwaiqjava.DTO;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AddressDTO {
+
+    @NotNull
+    private Integer teacher_id;
+    private String area;
+    private String street;
+    @Positive(message = "cannot be negative or zero")
+    private Integer building_number;
+
 }
