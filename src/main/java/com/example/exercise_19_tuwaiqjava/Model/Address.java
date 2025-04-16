a6b78d46afdb1899e9e4c78c8763dd58d746db86
aliali <aliali@test.com>
2025-04-16 18:55:58 +0300

    variables with validations

--- src/main/java/com/example/exercise_19_tuwaiqjava/Model/Address.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Model/Address.java
@@ -1,4 +1,34 @@
 package com.example.exercise_19_tuwaiqjava.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Address {
+
+
+    //Address Class :
+    //area , street , buildingNumber ( Add all required validation )
+    @Id
+    private Integer id;
+    @NotEmpty
+    private String area;
+    @NotEmpty
+    private String street;
+    @NotNull
+    @Positive(message = "cannot be negative or zero")
+    private Integer building_number;
+
+
+    private Integer teacher_id;
 }
--- src/main/java/com/example/exercise_19_tuwaiqjava/Model/Teacher.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Model/Teacher.java
@@ -1,4 +1,35 @@
 package com.example.exercise_19_tuwaiqjava.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Teacher {
+
+    //Teacher Class :
+    //id , name , age , email , salary ( Add all required validation )
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    private String name;
+    @NotNull
+    @Positive(message = "age should be greater than 1")
+    private Integer age;
+    @NotEmpty
+    @Email
+    private String email;
+    @NotNull
+    @PositiveOrZero(message = "salary cannot be negative")
+    private Integer salary;
+
 }
