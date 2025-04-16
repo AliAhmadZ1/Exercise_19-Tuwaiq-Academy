8c11820b1e15d8c3fc170965178427362be1e9d5
aliali <aliali@test.com>
2025-04-16 23:52:54 +0300

    JPA methods

--- src/main/java/com/example/exercise_19_tuwaiqjava/Repository/AddressRepository.java
+++ src/main/java/com/example/exercise_19_tuwaiqjava/Repository/AddressRepository.java
@@ -2,8 +2,11 @@ package com.example.exercise_19_tuwaiqjava.Repository;
 
 import com.example.exercise_19_tuwaiqjava.Model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
+    Address findAddressById(Integer id);
 
 }
